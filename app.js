14fc16f67160f7ba7ac3666ff22e00b685574e19
unknown <oraza7969@gmail.com>
2023-08-20 20:44:54 -0700

    Hello Dom

---
+++ app.js
@@ -0,0 +1,28 @@
+// Hello Dom
+
+// let paragraph = document.getElementById("myParagraph");
+
+// paragraph.textContent = "Hello, Dom";
+
+
+// Hidden Text
+
+// var paragraph = document.getElementById("hiddenText");
+
+// var button = document.getElementById("showTextButton");
+
+// button.addEventListener("click", function () {
+//     paragraph.style.display = "block";
+// });
+
+
+
+// Change background color
+
+var div = document.getElementById("myDiv");
+
+var button = document.getElementById("changeColorButton");
+
+button.addEventListener("click", function () {
+    div.style.backgroundColor = "blue";
+});
